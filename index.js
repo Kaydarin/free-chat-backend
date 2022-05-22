440b8735750c1297757ba4f32614d5d65640f50a
Kaydarin <zim50_killerz@yahoo.com>
2022-05-22 13:35:50 +0800

    Added CORS extension.

--- index.js
+++ index.js
@@ -2,20 +2,24 @@ import "dotenv/config.js";
 import HyperExpress from 'hyper-express';
 import Api from './routes/api.js';
 import Socket from './routes/socket.js'
+import cors from 'cors';
 
 const Server = new HyperExpress.Server();
 
-Server.listen(8100)
+Server.listen(process.env.PORT)
     .then((socket) => {
-        console.log('Webserver started on port 8100');
+        console.log('Webserver started on port ' + process.env.PORT);
         console.log(process.env.ENV);
     })
-    .catch((error) => console.log('Failed to start webserver on port 8100'));
+    .catch((error) => console.log('Failed to start webserver on port ' + process.env.PORT));
 
 Server.get('/', (request, response) => {
     response.send('Hello World');
 })
 
+Server.use(cors({
+    origin: '*'
+}));
 Server.use('/api', Api);
 
 // Websocket connections can now connect to '/ws/connect'
