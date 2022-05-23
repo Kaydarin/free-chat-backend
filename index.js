b755681946aa22557649968af1174fdfcffbf075
Kaydarin <zim50_killerz@yahoo.com>
2022-05-23 20:55:43 +0800

    Removed CORS extension.

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import "dotenv/config.js";
 import HyperExpress from 'hyper-express';
 import Api from './routes/api.js';
 import Socket from './routes/socket.js'
-import cors from 'cors';
 
 const Server = new HyperExpress.Server();
 
@@ -17,9 +16,6 @@ Server.get('/', (request, response) => {
     response.send('Hello World');
 })
 
-Server.use(cors({
-    origin: '*'
-}));
 Server.use('/api', Api);
 
 // Websocket connections can now connect to '/ws/connect'
